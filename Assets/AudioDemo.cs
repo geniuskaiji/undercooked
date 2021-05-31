5e011da2cf06f694d94fd8d9d784be0c336d105f
Dylan Fries <dylan.fries@connect.7oaks.org>
2021-05-31 09:38:02 -0500

    Fixed stove issues
    Fixed stove issues

--- Assets/AudioDemo.cs
+++ Assets/AudioDemo.cs
@@ -1,23 +1,28 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.Events;
+using UnityEngine.Events; // Method 2
 
 public class AudioDemo : MonoBehaviour
 {
-    public AudioClip anotherClip;
+    // Method 1
+    public AudioClip clip;
+    public AudioClip anotherClip; // source clip
+    public AudioSource playAudio; // plays the audio
 
-    public AudioSource playAudio;
-
+    // Method 2, uses events
     public UnityEvent audioEvent = new UnityEvent();
 
     public void Update() {
+        // Method 1
         if(Input.GetButtonDown("Jump")){
             playAudio.clip = anotherClip;
             playAudio.Play();
         }
 
+        // Method 2
         if(Input.GetKeyDown(KeyCode.A)){
+
             audioEvent.Invoke();
         }
     }
