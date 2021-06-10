2dbf6c69b24aa632dedc87c75829309f09194352
Dylan Fries <dylan.fries@connect.7oaks.org>
2021-06-10 10:05:33 -0500

    Scoring UI and UI Prefab for it.
    Updates to prefabs for ui and added scoring UI script basics

---
+++ Assets/ScoringUI.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoringUI : MonoBehaviour
+{
+    // Counters in UI, set them by assigning a String (or int converted to String) to the .text variable
+    public Text customerNumText;
+    public Text avgTimeText;
+    public Text currentTimeText;
+
+    // variables to store
+    public int currentCustomer = 0;
+    public int avgTime = 0; // how do we keep this accurate? Tricky problem here. 
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Assign to the Text UI elements
+        customerNumText.text = "" + currentCustomer;
+        avgTimeText.text = "" + avgTime;
+        currentTimeText.text = "" + 0;
+
+    }
+
+    // Call every frame in Update method
+    public void UpdateOrderTimer(int currentTimer){
+        currentTimeText.text = "" + currentTimer;
+    }
+
+    public void OrderCompleted(float timeItTook){
+        // convert to int
+
+        // add to average (a little tricky, normalize over the proportion it will contribute
+
+    }
+}
